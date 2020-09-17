e79f68006a6f9827421b8f9cbea1512506bc4bca
Shriya Hireholi <shriya.hireholi@mountblue.tech>
2020-09-17 05:20:08 +0000

    Update __init__.py

--- notes/__init__.py
+++ notes/__init__.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 username = os.getenv("username")
-password = os.getenv("username")
+password = os.getenv("password")
 
 DB_URL = f'postgresql://{username}:{password}@localhost/flasknotes'
 app = Flask(__name__)
