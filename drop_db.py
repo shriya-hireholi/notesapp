7f14c005ff7e96f2c2dac4d19b114b73fd47e5a8
Shriya Hireholi <shriya.hireholi@mountblue.tech>
2020-09-17 05:19:00 +0000

    Update drop_db.py

--- drop_db.py
+++ drop_db.py
@@ -2,4 +2,4 @@ from notes import username, password
 from sqlalchemy_utils import drop_database
 
 
-drop_database(f'postgresql://{username}:{password}@localhost/toyproject')
+drop_database(f'postgresql://{username}:{password}@localhost/flasknotes')
